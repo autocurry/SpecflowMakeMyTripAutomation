9c2278cbad97d462cf26eaf8a9cddb89debd31de
autocurry <sandeepsekhar555@gmail.com>
2020-09-17 14:31:13 +0530

    initial commit

---
+++ SpecflowMakeMyTripAutomation/SpecflowMakeMyTripAutomation/Features/Makemytriplogin.feature
@@ -0,0 +1,8 @@
+﻿Feature: Makemytriplogin
+verify that the login page functionalties are working as expected
+
+Scenario: verify that a registered user is able to login
+Given I am a registered makemytrip user
+When I open the login page
+And enter valid credentials
+Then i will be able to login successfully
---
+++ SpecflowMakeMyTripAutomation/SpecflowMakeMyTripAutomation/Steps/MakeMyTripLoginSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecflowMakeMyTripAutomation.Steps
+{
+    [Binding]
+    public class MakemytriploginSteps
+    {
+        [Given(@"I am a registered makemytrip user")]
+        public void GivenIAmARegisteredMakemytripUser()
+        {
+            Console.WriteLine("inside given");
+        }
+        
+        [When(@"I open the login page")]
+        public void WhenIOpenTheLoginPage()
+        {
+            Console.WriteLine("inside when");
+        }
+        
+        [When(@"enter valid credentials")]
+        public void WhenEnterValidCredentials()
+        {
+            Console.WriteLine("inside and");
+        }
+        
+        [Then(@"i will be able to login successfully")]
+        public void ThenIWillBeAbleToLoginSuccessfully()
+        {
+            Console.WriteLine("inside then");
+        }
+    }
+}
